ff3c7e963ebcb4a69cb4fe1562c3ad0f8898a8e1
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2025-05-21 21:45:22 -0400

    Fixed the backup script

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-CONTAINER="qrater_db_1"
-
-
-docker exec $CONTAINER \
-  /usr/bin/mysqldump --single-transaction -u root -p$(cat db-password.txt) qrater | gzip qrater_backup_$(date +%Y%m%d).sql.gz
+docker exec qrater-db-1 \
+  /usr/bin/mysqldump --single-transaction -u root -p$(cat db-password.txt) qrater | gzip > qrater_backup_$(date +%Y%m%d).sql.gz
